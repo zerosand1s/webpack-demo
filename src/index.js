ec8b09fbcb248db5677b21e317f7c28f6739aa15
Harshal <gangurdeharsh08@gmail.com>
2019-04-27 17:54:58 +0530

    load images into bundle

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
 import './style.css';
+import Octocat from './octocat.png';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'World'], ' ');
   element.classList.add('hello');
+
+  const myImage = new Image();
+  myImage.src = Octocat;
+
+  element.appendChild(myImage);
 
   return element;
 }
